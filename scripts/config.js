92f0a024ebd4e438cae81eb794f29e5489c5b7e2
bicknicktick <150901940+bicknicktick@users.noreply.github.com>
2025-09-29 09:43:46 +0700

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,9 @@
 window.prodigitalConfig = {
   checkoutUrls: {
-    "recap-yt": "http://lynk.id/leavefreehere/gr4qrz0mr6k9/checkout",
-    "youtube-analyzer": "http://lynk.id/leavefreehere/d48v69g0kwpx/checkout",
-    "atm-strategist": "http://lynk.id/leavefreehere/n3xyjy00kz4p/checkout", 
-    "pay-generator": "http://lynk.id/leavefreehere/9drey5ymdl7n/checkout",
+    "recap-yt": "https://products.bitzy.id",
+    "youtube-analyzer": "https://products.bitzy.id",
+    "atm-strategist": "https://products.bitzy.id", 
+    "pay-generator": "https://products.bitzy.id",
     "promo-media": "https://products.bitzy.id",
     "pitch-deck": "https://products.bitzy.id"
   }
